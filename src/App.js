2824e934cd9849171cf6c57bf139c2924a1b753b
Sree Teja <msreeteja24@gmail.com>
2025-11-21 23:26:26 +0530

    Added MoviesGrid component and used useState and useEffect.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className = "container">{
       //Here we are using the Header component imported from the Header.js file
       }<Header></Header>
+      <MoviesGrid></MoviesGrid>
       </div>
       <Footer></Footer>
     </div>
