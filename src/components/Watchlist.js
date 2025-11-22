1b5eb17b91dc7f4df5bc0f7e4c2b89e9d57ed63e
Sree Teja <msreeteja24@gmail.com>
2025-11-22 22:53:03 +0530

    Building the Watchlisht componet

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
-export default function Watchlist(movies, watchlist, toggleWactlist) {
-  return <div className="title">Your Watchlist</div>;
+export default function Watchlist({ movies, watchlist, toggleWactlist }) {
+  return (
+    <div>
+      <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchlist={toggleWactlist}
+              isWatchlisted={true}
+            ></MovieCard>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
