c516eba9c027483d8ac74ca2c35c382dc178293a
Sree Teja <msreeteja24@gmail.com>
2025-11-22 21:03:01 +0530

    Creating the watchlist component and configuring the route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
     <div className="App">
-      <div className = "container">{
-      //Here we are using the Header component imported from the Header.js file
-      }<Header></Header>
-      <MoviesGrid></MoviesGrid>
+      <div className="container">
+        {
+          //Here we are using the Header component imported from the Header.js file
+        }
+        <Header></Header>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid></MoviesGrid>}></Route>
+            <Route path="/watchlist" element={<Watchlist></Watchlist>}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>
---
+++ src/components/Watchlist.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles.css";
+
+export default function Watchlist() {
+  return <div className="title">Your Watchlist</div>;
+}
