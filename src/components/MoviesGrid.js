5307f86d0b4f25f9dfd4eed8506ca07c9e0fa96b
Sree Teja <msreeteja24@gmail.com>
2025-11-22 10:49:03 +0530

    Rendering the movies in card

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -12,5 +12,22 @@ export default function MoviesGrid(){
     
     }, []);
 
-        return(<div>{movies.length}</div>);
+        return(
+        <div className= 'movies-grid'>
+            {
+                movies.map(movie => (
+                    <div key={movie.id} className= 'movie-card'>
+                        <img src={`images/${movie.image}`} alt ={movie.title}/>
+                        <div className='movie-card-info'>
+                            <h3 className= 'movie-card-title'> {movie.title}</h3>
+                            <p className= 'movie-card-genre'> {movie.genre}</p>
+                            <p className= 'movie-card-rating'> {movie.rating}</p>
+                            </div>
+                    </div>
+                ))
+                
+            }
+
+        </div>
+    );
 }
