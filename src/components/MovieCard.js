b70c9048ef7ee6b1aae88f8c0e2e41b9fdb3149e
Sree Teja <msreeteja24@gmail.com>
2025-11-22 13:56:31 +0530

    Dynamic rating color

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,6 +6,16 @@ export default function MovieCard({movie}){
     const handleError = (e) => 
         e.target.src = "images/default.jpg";
 
+    const getRatingClass = (rating) => {
+        if(rating >=8){
+            return "rating-good";
+        } else if(rating >=5 && rating<8){
+            return "rating-ok";
+        }else{
+            return "rating-bad";
+        }
+    }
+
     return (
         <div key={movie.id} className = 'movie-card'>
             <img 
@@ -14,7 +24,7 @@ export default function MovieCard({movie}){
             onError={handleError}/>
             <h3 className= 'movie-card-title'> {movie.title} </h3>
             <p className = 'movie-card-genre' >{movie.genre}</p>
-            <p className = 'movie-card-rating'> {movie.rating}</p>
+            <p className = {`movie-card-rating ${getRatingClass(movie.rating)}`}> {movie.rating}</p>
         </div>
     );
 }
