239cf6f16289c01fc839e36e066cc89b63a04008
Sree Teja <msreeteja24@gmail.com>
2025-11-22 22:09:36 +0530

    Adding state to watchlist (toggleWatchlistfunction)

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
 
   useEffect(() => {
     fetch("movies.json") //Here since we are in the same directory we are not writing the whole URL
       .then((response) => response.json()) //here we are converting the response to Json (later they will be available as Javascript objects)
       .then((data) => setMovies(data)); //Here we are assigning them to the data objects.
   }, []);
+
+  //Here prev is the previous state
+  /*So if the previous state includes the movieId then we will remove the id, other wise
+  we us the spread operator and append the new movieId .
+  Here we used !== to remove the existing moveie id*/
+  const toogleWatchlist = (movieId) => {
+    setWatchlist((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
 
   return (
     <div className="App">
